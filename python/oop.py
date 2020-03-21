57d1d5748c89104df9b7849007428a3e1787cd81
guilherme.sesterheim@gmail.com <guilherme.sesterheim@gmail.com>
2020-03-21 16:33:59 -0400

    python e git ignore

---
+++ python/oop.py
@@ -0,0 +1,22 @@
+class Math:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+
+    def subtract(self):
+        return self.x - self.y
+
+    def multiply(self):
+        return self.x * self.y
+
+    def divide(self):
+        return self.x / self.y
+
+m = Math(2, 3)
+print(m.add())
+print(m.subtract())
+print(m.multiply())
+print(m.divide())
