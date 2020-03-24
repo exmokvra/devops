3ccf498772f904809435886fec47eb3a9cbb43cc
guisesterheim <guilherme.sesterheim@gmail.com>
2020-03-23 20:32:08 -0400

    go start

---
+++ go/web-sample.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Go HTTP Service running like a charm")
+}
+
+func main(){
+	fmt.Print("Serving at localhost")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
